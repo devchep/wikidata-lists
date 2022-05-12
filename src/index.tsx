bfe2a87aabac4f4b5bc2f8ac7ffc7dce79de165b
devchep <aschepushtanov@gmail.com>
2022-05-13 00:51:41 +0300

    :lipstick: add index.scss (kind of reset.css)

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import Footer from './Layouts/Footer';
 import Header from './Layouts/Header';
 import Main from './Layouts/Main';
+import './index.scss'
 
 function Index() {
     return (
